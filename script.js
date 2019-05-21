3eae6f5eaa6ab5339f138910e8cc2dbf68c573f4
christopherleveston <christopherleveston@outlook.com>
2019-05-21 16:47:57 -0500

    fixed alignment of the front page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /** fade in steak image on page load */
-var logoEl = document.getElementById("steakImg");
+const logoEl = document.getElementById("steakImg");
 $(document).ready(function(){
     $(logoEl).hide().delay(500).fadeIn(1400);
 });
@@ -8,34 +8,42 @@ $(document).ready(function(){
  * dish. */
 
  /** Access the 'Hot Dishes' title */
- var dishNameHeader = document.getElementById("dishName");
+ const dishNameHeader = document.getElementById("dishName");
  dishNameHeader.setAttribute("class", "black");
- var priceEl  = document.getElementById("Price");
+ const priceEl  = document.getElementById("Price");
 
  /** On hover on ads, change title */
-var topDishes = document.getElementsByClassName("boxes");
-function adOneHover() {
-    dishNameHeader.textContent = "The Rock";
-    
-        priceEl.textContent = "$20";
+const topDishes = document.getElementsByClassName("boxes");
+const defaultAdText = "Hot Dishes";
+const adText = ["The Rock", "The Steak Garden", "Icy Mountain"];
+let adPrice = ["20", "$10", "$5"];
+
+
+const adOneHover = () => {
+    dishNameHeader.textContent = adText[0];
+        priceEl.textContent = adPrice[0];
 }
 topDishes[0].addEventListener("mouseover", adOneHover, false);
-function adTwoHover() {
-    dishNameHeader.textContent = "The Steak Garden";
 
-        priceEl.textContent = "$10";
+
+const adTwoHover = () => {
+    dishNameHeader.textContent = adText[1];
+        priceEl.textContent = adPrice[1];
 }
 topDishes[1].addEventListener("mouseover", adTwoHover, false);
-function adThreeHover() {
-    dishNameHeader.textContent = "Icy Mountain";
- 
-        priceEl.textContent = "$5";
+
+
+const adThreeHover = () => {
+    dishNameHeader.textContent = adText[2];
+        priceEl.textContent = adPrice[2];
 }
 topDishes[2].addEventListener("mouseover", adThreeHover,false);
 
-for(var i=0; i < topDishes.length; i++) {
+
+
+for(let i = 0; i < topDishes.length; i += 1) {
     function changeBack() {
-        dishNameHeader.textContent = "Hot Dishes!"
+        dishNameHeader.textContent = defaultAdText;
         $(function(){
             $(dishNameHeader).animate({
                 paddingLeft: "0px"
